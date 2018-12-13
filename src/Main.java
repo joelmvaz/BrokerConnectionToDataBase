5e00c10767b87e7a308648dc80c7edf1996f8188
Joel Vaz <joelmatosvaz@gmail.com>
2018-12-13 18:59:02 +0000

    Added serial port reading code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-public class Main{
+/*public class Main{
     public static void main(String args[]) throws Exception{
         Main m = new Main();
-        JDBC.connect();
+        //JDBC.connect();
 
         //MDN sql prepare
-        
-        JDBC.WriteStringToDataBase("INSERT INTO plugnplay VALUES(DEFAULT, '50', '75', CURRENT_TIMESTAMP);");
+
+        //JDBC.WriteStringToDataBase("INSERT INTO plugnplay VALUES(DEFAULT, '50', '75', CURRENT_TIMESTAMP);");
     }
-}
+}*/
